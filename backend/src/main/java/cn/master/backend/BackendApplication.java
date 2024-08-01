1040eca5796af770fa7e8df7dd97ef4c5e480122
lijinglin <the2ndindec@gmail.com>
2024-08-01 10:17:52 +0800

    feat(banner): 取消显示banner

--- backend/src/main/java/cn/master/backend/BackendApplication.java
+++ backend/src/main/java/cn/master/backend/BackendApplication.java
@@ -1,13 +1,19 @@
 package cn.master.backend;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author the2n
+ */
 @SpringBootApplication
 public class BackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
+        SpringApplication application = new SpringApplication(BackendApplication.class);
+        application.setBannerMode(Banner.Mode.OFF);
+        application.run(args);
     }
 
 }
