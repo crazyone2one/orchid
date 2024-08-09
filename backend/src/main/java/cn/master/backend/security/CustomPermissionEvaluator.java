0397e2664f5395c781997c2dc97ff5b4042c021f
lijinglin <the2ndindec@gmail.com>
2024-08-09 16:28:48 +0800

    refactor(CustomPermissionEvaluator): 优化权限认证方法

--- backend/src/main/java/cn/master/backend/security/CustomPermissionEvaluator.java
+++ backend/src/main/java/cn/master/backend/security/CustomPermissionEvaluator.java
@@ -4,8 +4,7 @@ import cn.master.backend.entity.UserRolePermission;
 import cn.master.backend.service.BaseUserRolePermissionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
-import org.aopalliance.intercept.MethodInvocation;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.access.PermissionEvaluator;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -13,7 +12,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -31,6 +29,9 @@ public class CustomPermissionEvaluator implements PermissionEvaluator {
         if ((Objects.isNull(authentication)) || Objects.isNull(target) || !(permission instanceof String)) {
             return false;
         }
+        if ("administrator".equals(authentication.getName())) {
+            return true;
+        }
         if (!(target instanceof String)) {
             return false;
         }
@@ -43,18 +44,20 @@ public class CustomPermissionEvaluator implements PermissionEvaluator {
         if (Objects.isNull(authentication) || (Objects.isNull(targetType)) || (targetId == null) || !(permission instanceof String)) {
             return false;
         }
+        if ("administrator".equals(authentication.getName())) {
+            return true;
+        }
         return hasPrivilege(authentication, targetType.toUpperCase(), permission.toString().toUpperCase());
     }
 
     private boolean hasPrivilege(Authentication authentication, String target, String permission) {
-        if ("administrator".equals(authentication.getName())) {
-            return true;
-        }
         List<UserRolePermission> rolePermissions = baseUserRolePermissionService.getPermissions(extractRoles(authentication));
         if (rolePermissions.isEmpty()) {
             return false;
         }
-        return rolePermissions.stream().anyMatch(rolePermission -> rolePermission.getPermissionId().equals(target + ":" + permission));
+        return rolePermissions.stream()
+                .anyMatch(rolePermission ->
+                        rolePermission.getPermissionId().equals(Objects.isNull(target) ? permission : target + ":" + permission));
     }
 
     private List<String> extractRoles(Authentication auth) {
@@ -62,22 +65,35 @@ public class CustomPermissionEvaluator implements PermissionEvaluator {
     }
 
     public boolean isAdmin() {
-        // TODO: Implement
-        return true;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return "administrator".equals(authentication.getName());
     }
 
     public boolean hasAuthority(String authority) {
-        return hasAnyAuthority(authority);
-    }
-
-    public boolean hasAnyAuthority(String... authority) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        List<UserRolePermission> rolePermissions = baseUserRolePermissionService.getPermissions(extractRoles(authentication));
-        if (rolePermissions.isEmpty()) {
+
+        if (Objects.isNull(authentication) || StringUtils.isBlank(authority)) {
             return false;
         }
-        List<String> list = rolePermissions.stream().map(UserRolePermission::getPermissionId).toList();
-        val tmpAuthorities = Arrays.asList(authority[0].split(","));
-        return tmpAuthorities.stream().anyMatch(list::contains);
+        if ("administrator".equals(authentication.getName())) {
+            return true;
+        }
+        return hasPrivilege(authentication, null, authority);
+    }
+
+    public boolean hasAnyAuthority(String authorities) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (Objects.isNull(authentication) || StringUtils.isBlank(authorities)) {
+            return false;
+        }
+        if ("administrator".equals(authentication.getName())) {
+            return true;
+        }
+        for (String authority : authorities.split(",")) {
+            if (StringUtils.isNoneBlank(authority) && hasPrivilege(authentication, null, authority)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
