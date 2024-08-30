d950e0bc07180abfcfdd0743208e7d38c1cdfa67
lijinglin <the2ndindec@gmail.com>
2024-08-30 14:42:30 +0800

    refactor(鉴权): NoResourceFoundException

--- backend/src/main/java/cn/master/backend/handler/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/cn/master/backend/handler/exception/GlobalExceptionHandler.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -37,6 +38,13 @@ public class GlobalExceptionHandler {
             errors.put(fieldName, errorMessage);
         });
         return ResultHolder.error(HttpResultCode.VALIDATE_FAILED.getCode(), HttpResultCode.VALIDATE_FAILED.getMessage(), errors);
+    }
+
+    @ExceptionHandler(value = NoResourceFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<ResultHolder> handleNoResourceFoundException(NoResourceFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ResultHolder.error(HttpResultCode.NOT_FOUND.getCode(), HttpResultCode.NOT_FOUND.getMessage()));
     }
 
     @ExceptionHandler(MSException.class)
